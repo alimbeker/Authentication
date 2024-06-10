6ea83ce569ad861a106ea50d6ae02d4b319891c3
amiralimbekerov <asuraaka@outlook.com>
2024-06-10 17:34:56 +0500

    translate to compose

--- app/src/main/java/relog/android/authentication/ui/theme/auth/AuthActivity.kt
+++ app/src/main/java/relog/android/authentication/ui/theme/auth/AuthActivity.kt
@@ -2,28 +2,49 @@ package relog.android.authentication.ui.theme.auth
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.google.firebase.auth.FirebaseAuth
 import dagger.hilt.android.AndroidEntryPoint
-import relog.android.authentication.R
+import relog.android.authentication.ui.theme.auth.fragments.LoginScreen
 import relog.android.authentication.ui.theme.main.MainActivity
 
 @AndroidEntryPoint
-class AuthActivity : AppCompatActivity() {
+class AuthActivity : ComponentActivity() {
+
+    private val auth = FirebaseAuth.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_auth)
-
+        setContent {
+            val navController = rememberNavController()
+            AuthNavHost(navController)
+        }
     }
 
     override fun onStart() {
         super.onStart()
+        if (auth.currentUser != null) {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
 
-        if (FirebaseAuth.getInstance().currentUser != null) {
-            Intent(this, MainActivity::class.java).also {
-                startActivity(it)
-                finish()
-            }
+@Composable
+fun AuthNavHost(navController: NavHostController) {
+    NavHost(navController = navController, startDestination = "login") {
+        composable("login") {
+            LoginScreen(navController = navController)
+        }
+        composable("register") {
+            RegisterScreen(navController = navController)
         }
     }
 }
